840df9e254821afbabe8f894871e07152515867c
karti <karti@LAPTOP-VFIQ5OQ8>
2020-05-11 10:15:56 +0530

    Ok

--- src/Testing/AllTests.java
+++ src/Testing/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({testAddNumbers.class,testAddStrings.class})
+@SuiteClasses({testAddNumbers.class, testAddStrings.class})
 public class AllTests {
 
 }
